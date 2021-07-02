4ee8562f0e951b8d4464931ab890c8cc5c310774
TheSmartHyena <skopal.philippe@gmail.com>
2021-07-02 11:35:22 +0200

    Serve react app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CalcModule } from './calc/calc.module';
 import { MathService } from './calc/math.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CalcModule],
+  imports: [CalcModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'build'),
+    renderPath: "/",
+  })],
   controllers: [AppController],
   providers: [AppService, MathService],
+  
 })
 export class AppModule {}
